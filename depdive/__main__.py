099fcbff59bc81bfa747593a4b27ab7d96d2f029
Nasif Imtiaz <nasifimtiaz88@gmail.com>
2022-01-28 23:43:49 -0500

    Bump version from 0.0.29 to 0.0.30

--- depdive/__main__.py
+++ depdive/__main__.py
@@ -5,7 +5,7 @@ from rich import traceback
 
 
 @click.command()
-@click.version_option(version="0.0.29", message=click.style("depdive Version: 0.0.29"))
+@click.version_option(version="0.0.30", message=click.style("depdive Version: 0.0.30"))
 def main() -> None:
     """depdive."""
 
