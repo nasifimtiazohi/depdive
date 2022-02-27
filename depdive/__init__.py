5d47c3c6210941deb4a9185890d1c1ea6d5d2ee0
Nasif Imtiaz <nasifimtiaz88@gmail.com>
2022-02-27 13:00:49 -0500

    Bump version from 0.0.37 to 0.0.38

--- depdive/__init__.py
+++ depdive/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = "Nasif Imtiaz"
 __email__ = "nasifimtiaz88@gmail.com"
-__version__ = "0.0.37"
+__version__ = "0.0.38"
--- depdive/__main__.py
+++ depdive/__main__.py
@@ -5,7 +5,7 @@ from rich import traceback
 
 
 @click.command()
-@click.version_option(version="0.0.37", message=click.style("depdive Version: 0.0.37"))
+@click.version_option(version="0.0.38", message=click.style("depdive Version: 0.0.38"))
 def main() -> None:
     """depdive."""
 
